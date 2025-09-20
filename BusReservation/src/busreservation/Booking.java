3233594e37c11001670804a25e2ad67f4b510c65
brindhac123 <84434467+brindhac123@users.noreply.github.com>
2025-09-20 18:57:53 +0530

    Update Booking.java

--- BusReservation/src/busreservation/Booking.java
+++ BusReservation/src/busreservation/Booking.java
@@ -1,16 +1,14 @@
 package busreservation;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
+import java.util.*;
 public class Booking {
 		// TODO Auto-generated method stub
 		String p_name;
 		int B_no;
 		Date date;
 		Scanner sc=new Scanner(System.in);
-		Booking() throws ParseExceptio
-		n{
+		Booking() throws ParseException{
 		System.out.println("Enter passenger name ");
 		p_name=sc.next();
 		System.out.println("Enter Bus no ");
@@ -18,8 +16,23 @@ public class Booking {
 		System.out.println("Enter Date dd-mm-yyyy");
 		String dateinput = sc.next();
 		SimpleDateFormat dateformat=new SimpleDateFormat("dd-MM-yyyy");
-		date=dateformat.parse(dateinput);
-		public boolean isAvailable( )
+		date=dateformat.parse(dateinput);}
+		public boolean isAvailable(ArrayList<Booking> bookings,ArrayList<Bus> buses) {
+			int capacity=0;
+			for(Bus bus:buses) {
+				if(bus.getB_no()==B_no) {
+					capacity=bus.getB_no();
+					}
+			}
+			int booked=0;
+			for(Booking b: bookings) {
+				if(b.B_no==B_no&&b.date.equals(date)) {
+					booked++;
+				}
+			}
+			return booked>capacity ? false:true;
+			//return false;
+			
 		}
 
 }
